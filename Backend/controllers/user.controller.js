939b951b6cb09ad98cba46881c688c99a6ef6330
Yuliy Sherman <yuliy.sherman@gmail.com>
2025-12-30 11:32:30 +0200

    feature/delete user

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,7 +1,18 @@
-import User from '../models/user.module.js';
-
+import { email, jwt } from "zod";
+import User from "../models/user.module.js";
 
 export const getAll = async (req, res) => {
-    const users = await User.find().select("-password");
-    return res.status(200).json({success: true, users});
-}
+  const users = await User.find().select("-password");
+  return res.status(200).json({ success: true, users });
+};
+export const DeleteUser = async (req, res) => {
+  try {
+    const result = await User.deleteOne({ email: req.user.email });
+  } catch (error) {
+    return res.status(500).json({ message: "unexepted error"});
+  }
+  if (result.deletedCount === 0) {
+    return res.status(404).json({ message: "User not found" });
+  }
+  return res.status(200).json({ message: "User deleted successfully" });
+};
